7ccfa643c70c02225f773e8f5b703a777f0af6d9
vToMy <tomkedem@gmail.com>
2024-06-22 20:43:21 +0300

    added typing to observable_config.py

--- pydataconfig/observable_config.py
+++ pydataconfig/observable_config.py
@@ -1,3 +1,5 @@
+from typing import Callable, Any, Awaitable, Generator, Self
+
 import re
 
 import inspect
@@ -6,16 +8,20 @@ import threading
 import asyncio
 
 
-class Event:
+type CallbackType[**P] = Callable[P, Any] | Callable[P, Awaitable[Any]]
+type TimeoutType = int | float
+
+
+class Event[**P]:
     def __init__(self):
-        self.sync_callbacks = []
-        self.async_callbacks = []
+        self.sync_callbacks: list[Callable[P, Any]] = []
+        self.async_callbacks: list[Callable[P, Awaitable[Any]]] = []
         self.thread_condition = threading.Event()
         self.asyncio_condition = asyncio.Event()
-        self.last_args = None
-        self.last_kwargs = None
+        self.last_args: P.args = None
+        self.last_kwargs: P.kwargs = None
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> None:
         self.clear()
         for callback in self.sync_callbacks:
             callback(*args, **kwargs)
@@ -25,56 +31,61 @@ class Event:
         self.last_kwargs = kwargs
         self.set()
 
-    def __iadd__(self, callback):
+    def __iadd__(self, callback: CallbackType[P]) -> Self:
         if inspect.iscoroutinefunction(callback):
             self.async_callbacks.append(callback)
         else:
             self.sync_callbacks.append(callback)
         return self
 
-    def __isub__(self, callback):
+    def __isub__(self, callback: CallbackType[P]) -> Self:
         if inspect.iscoroutinefunction(callback):
             self.async_callbacks.remove(callback)
         else:
             self.sync_callbacks.remove(callback)
         return self
 
-    def __await__(self):
+    def __await__(self) -> Generator[Any, None, tuple[P.args, P.kwargs]]:
         return self.wait_async().__await__()
 
-    async def wait_async(self):
+    async def wait_async(self) -> tuple[P.args, P.kwargs]:
         await self.asyncio_condition.wait()
         return self.last_args, self.last_kwargs
 
-    def wait(self, timeout=None):
+    def wait(self, timeout: TimeoutType = None) -> tuple[P.args, P.kwargs]:
         self.thread_condition.wait(timeout)
         return self.last_args, self.last_kwargs
 
-    def set(self):
+    def set(self) -> None:
         self.thread_condition.set()
         self.asyncio_condition.set()
 
-    def clear(self):
+    def clear(self) -> None:
         self.thread_condition.clear()
         self.asyncio_condition.clear()
 
 
-class ObservableConfig:
-    def __getattr__(self, item):
-        match = re.match(r'on_(.+)_changed', item)
-        if match:
-            key = match.group(1)
-            if not hasattr(self, key):
-                raise AttributeError(f'No field called: {key} found for: {item}')
-            event = Event()
-            super().__setattr__(item, event)
-            return event
-        raise AttributeError(item)
+ON_CHANGED_PATTERN = re.compile(r'on_(.+)_changed')
+ON_CHANGED_FORMAT = 'on_{name}_changed'
 
-    def __setattr__(self, key, value):
-        previous_value = getattr(self, key)
-        super().__setattr__(key, value)
-        if value != previous_value:
-            event = getattr(self, f'on_{key}_changed', None)
-            if isinstance(event, Event):
-                event(previous_value, value)
+
+class ObservableConfig:
+    def __getattr__(self, name: str) -> Any:
+        match = re.match(ON_CHANGED_PATTERN, name)
+        if not match:
+            raise AttributeError(name)
+        key = match.group(1)
+        if not hasattr(self, key):
+            raise AttributeError(f'No field called: {key} found for: {name}')
+        event = Event()
+        super().__setattr__(name, event)
+        return event
+
+    def __setattr__(self, name: str, value: Any):
+        previous_value = getattr(self, name)
+        super().__setattr__(name, value)
+        if value == previous_value:
+            return
+        event = getattr(self, ON_CHANGED_FORMAT.format(name=name), None)
+        if isinstance(event, Event):
+            event(previous_value, value)
